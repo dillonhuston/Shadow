69a4f85c864f652c32260169df8f2a5c5fe9cc96
user <d9392828@example.com>
2026-06-19 16:48:13 +0100

    Finally the last commit. Had to ensure user_router was included, rather than commented out (silly me)

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import re
 from fastapi import FastAPI, APIRouter
 from app.routers.auth import router as auth_router
 from app.routers.file import router as file_router
-#from app.routers.user import router as user_router
+from app.routers.user import router as user_router
 
 app = FastAPI()
 
 app.include_router(auth_router)
 app.include_router(file_router)
-#app.include_router(user_router)
+app.include_router(user_router)
 
 
 @app.get('/health')
